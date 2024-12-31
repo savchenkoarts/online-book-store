58dc2e47cbb076dc7ff46795918750850d0b303d
Oleksandr Savchenko <savchenko.arts@gmail.com>
2024-12-31 18:54:55 +0200

    Add CustomGlobalExceptionHandler class (#5)
    * Add CustomGlobalExceptionHandler class
    Add meaningful validation for existing DTO classes
    
    * Fixed after first review
    
    * Fixed after review

--- src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,11 +1,19 @@
 package mate.academy.onlinebookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public record CreateBookRequestDto(
+        @NotBlank
         String title,
+        @NotBlank
         String author,
+        @NotBlank
         String isbn,
+        @NotNull
+        @Min(0)
         BigDecimal price,
         String description,
         String coverImage
--- src/main/java/mate/academy/onlinebookstore/model/Book.java
+++ src/main/java/mate/academy/onlinebookstore/model/Book.java
@@ -6,30 +6,38 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
-@Data
+@Getter
+@Setter
 @Entity
-@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
-@Where(clause = "is_deleted=false")
+@SQLDelete(sql = "UPDATE books SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
 @Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
+    @NotBlank
     private String title;
     @Column(nullable = false)
+    @NotBlank
     private String author;
     @Column(nullable = false, unique = true)
+    @NotBlank
     private String isbn;
     @Column(nullable = false)
+    @NotNull
     private BigDecimal price;
     private String description;
     private String coverImage;
     @Column(nullable = false)
-    private boolean isDeleted = false;
+    private boolean deleted = false;
 }
