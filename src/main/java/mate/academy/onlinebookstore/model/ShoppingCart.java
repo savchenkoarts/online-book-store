c988e1edf61add663d5316ada3f27633d6e02d85
Oleksandr Savchenko <savchenko.arts@gmail.com>
2026-06-12 22:51:24 +0200

    After fixed v.2

--- src/main/java/mate/academy/onlinebookstore/model/ShoppingCart.java
+++ src/main/java/mate/academy/onlinebookstore/model/ShoppingCart.java
@@ -4,8 +4,6 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
@@ -29,7 +27,6 @@ import org.hibernate.annotations.SQLRestriction;
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @MapsId
